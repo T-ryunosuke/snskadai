1066fc87895df3195f55e9112270d36e8d7fb9bf
tokunaga_ryunosuke <kira.toyp@gmail.com>
2022-12-20 16:42:04 +0900

    アコーディオン機能修正 フォロワーリスト遷移・フォローリスト遷移・search遷移追加

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -28,20 +28,15 @@
         <div id = "head">
         <h1><a href="/top"><img src="images/atlas.png"></a></h1>
     <!--アコーディオン追加↓-->
-    <div class="section s_02">
-    <div class="accordion_one">
-      <div class="accordion_header">アコーディオン、一つ開けると他は閉じる
-        <div class="i_box"><i class="one_i"></i></div>
-      </div>
-      <div class="accordion_inner">
-        <div class="box_one">
-          <p class="txt_a_ac">アコーディオンの中身です。</p>
+    <div class="section s_01">
+      <div class="accordion_one">
+        <div class="accordion_header">menu
+          <div class="i_box"><i class="one_i"></i></div>
         </div>
-      </div>
-    </div>
-    </div>
-    <!--アコーディオン追加↑-->
-            <div id="">
+        <div class="accordion_inner">
+          <div class="box_one">
+            <p class="txt_a_ac">
+              <div id="">
                 <div id="">
                     <p>さん<img src="images/icon1.png"></p>
                 <div>
@@ -50,7 +45,13 @@
                     <li><a href="/profile">プロフィール</a></li>
                     <li><a href="/logout">ログアウト</a></li>
                 </ul>
-            </div>
+              </div>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--アコーディオン追加↑-->
         </div>
     </header>
     <div id="row">
@@ -64,14 +65,14 @@
                 <p>フォロー数</p>
                 <p>〇〇名</p>
                 </div>
-                <p class="btn"><a href="">フォローリスト</a></p>
+                <p class="btn"><a href="/follow-list">フォローリスト</a></p>
                 <div>
                 <p>フォロワー数</p>
                 <p>〇〇名</p>
                 </div>
-                <p class="btn"><a href="">フォロワーリスト</a></p>
+                <p class="btn"><a href="/follower-list">フォロワーリスト</a></p>
             </div>
-            <p class="btn"><a href="">ユーザー検索</a></p>
+            <p class="btn"><a href="/search">ユーザー検索</a></p>
         </div>
     </div>
     <footer>
