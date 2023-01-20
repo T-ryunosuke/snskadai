a0d44e3c964459b062732d0185c35444759cf163
tokunaga_ryunosuke <kira.toyp@gmail.com>
2023-01-20 13:06:11 +0900

    投稿機能追加のための記述

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //↓投稿機能追加記述 user.phpと連携
+    public function user() { //1対多の「１」側なので単数系
+      return $this->belongsTo('App\User');
+    }
 }
