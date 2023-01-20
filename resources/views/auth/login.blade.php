da003ab3efacb12baea8b32a8f60324c952dcc02
tokunaga_ryunosuke <kira.toyp@gmail.com>
2023-01-20 13:14:50 +0900

    レイアウト微調整

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,14 +5,17 @@
 {!! Form::open(['url' => '/login']) !!}
 
 <p>AtlasSNSへようこそ</p>
-
-{{ Form::label('e-mail') }}
+<br><br>
+{{ Form::label('mail adress') }}
+<br>
 {{ Form::text('mail',null,['class' => 'input']) }}
+<br><br>
 {{ Form::label('password') }}
+<br>
 {{ Form::password('password',['class' => 'input']) }}
-
+<br>
 {{ Form::submit('ログイン') }}
-
+<br><br><br>
 <p><a href="/register">新規ユーザーの方はこちら</a></p>
 
 {!! Form::close() !!}
