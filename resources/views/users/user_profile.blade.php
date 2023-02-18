42d588b52cdea686f8990475a1761fa22d2a773e
tokunaga_ryunosuke <kira.toyp@gmail.com>
2023-02-18 12:43:12 +0900

    変数の訂正

--- resources/views/users/user_profile.blade.php
+++ resources/views/users/user_profile.blade.php
@@ -3,21 +3,21 @@
 @section('content')
 <div class="top-page">
     <section class="user-profile">
-      @foreach($posts as $post)
-        <img src=" {{ asset('storage/avatar/'.$post->user->images) }} " class="rounded-circle" style="object-fit: cover; width: 40px; height: 40px;">
-        <p>name    {{ $post->user->username }}</p>
+      @foreach($profile as $profile)
+        <img src=" {{ asset('storage/avatar/'.$profile->images) }} " class="rounded-circle" style="object-fit: cover; width: 40px; height: 40px;">
+        <p>name    {{ $profile->username }}</p>
 
-        <p>bio    {{ $post->user->bio }}</p>
+        <p>bio    {{ $profile->bio }}</p>
 
-        @if (Auth::user()->isFollowing($post->user->id))
-        <p class="unfollow-btn"><a href="{{ $post->user->id }}/unfollow">フォロー解除</a></p>
+        @if (Auth::user()->isFollowing($profile->id))
+        <p class="unfollow-btn"><a href="{{ $profile->id }}/unfollow">フォロー解除</a></p>
         @else
-        <p class="follow-btn"><a href="{{ $post->user->id }}/follow">フォローする</a></p>
+        <p class="follow-btn"><a href="{{ $profile->id }}/follow">フォローする</a></p>
         @endif
 
       @endforeach
     </section>
-
+</div>
     <section class="followPost">
       @foreach($posts as $post)
         <img src=" {{ asset('storage/avatar/'.$post->user->images) }} " class="rounded-circle" style="object-fit: cover; width: 40px; height: 40px;">
@@ -25,5 +25,5 @@
         <p>{{ $post->post }}</p>
       @endforeach
     </section>
-</div>
+
 @endsection
